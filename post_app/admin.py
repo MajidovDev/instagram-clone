b41759b968a1057c1cd778a11e812d1c622cd220
Sherzod Majidov <majidovdev@mail.ru>
2023-05-26 02:32:18 +0500

    Models are registered through ModelAdmin

--- post_app/admin.py
+++ post_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from post_app.models import PostModel, PostLikeModel, PostCommentModel, CommentLikeModel
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'caption', 'created_time')
+    search_fields = ('id', 'author__username', 'caption')
+
+
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'created_time')
+    search_fields = ('id', 'author__username', 'comment')
+
+
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'created_time')
+    search_fields = ('id', 'author__username')
+
+
+class CommentLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'created_time')
+    search_fields = ('id', 'author__username')
+
+
+admin.site.register(PostModel, PostAdmin)
+admin.site.register(PostCommentModel, PostCommentAdmin)
+admin.site.register(PostLikeModel, PostLikeAdmin)
+admin.site.register(CommentLikeModel, CommentLikeAdmin)
